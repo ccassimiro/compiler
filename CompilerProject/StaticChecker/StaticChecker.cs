0e5ecf6a29b3316a046fc8c4e91932ef02d5f7bf
lcassimiro1 <lcassimiro1@gmail.com>
2020-06-16 11:39:50 -0300

    Ajustes na estrutura do código

--- CompilerProject/StaticChecker/StaticChecker.cs
+++ CompilerProject/StaticChecker/StaticChecker.cs
@@ -9,61 +9,117 @@ namespace StaticChecker
 {
     public class StaticChecker
     {
+        public static IDictionary<string, string> reservedWords = new Dictionary<string, string>();
+        public static IDictionary<string, string> reservedSymbols = new Dictionary<string, string>();
+        public static IDictionary<string, string> reservedTypes = new Dictionary<string, string>();
+
         static void Main(string[] args)
         {
-            int stillInProgram = 1;
+            string documentNameOrPath, path;
 
 
-            while (stillInProgram == 1)
+            Console.WriteLine("Digite o nome do arquivo ou caminho completo: ");
+            documentNameOrPath = Console.ReadLine();
+
+            var splittedName = documentNameOrPath.Split('\\');
+
+            //"inteligência" para saber se foi informado um path completo ou apenas o nome do documento.
+            if (splittedName.Length > 1)
             {
-                string documentNameOrPath, path;
-                List<String> lines = new List<String>();
-
-                Console.WriteLine("Digite o nome do arquivo ou caminho completo: ");
-                documentNameOrPath = Console.ReadLine();
-
-                var splittedName = documentNameOrPath.Split('\\');
-
-                //"inteligência" para saber se foi informado um path completo ou apenas o nome do documento.
-                if (splittedName.Length > 1)
-                {
-                    path = documentNameOrPath;
-                }
-                else
-                {
-                    path = documentNameOrPath + ".201";
-                }
-
-                try
-                {
-                    using (StreamReader sr = new StreamReader(@path))
-                    {
-                        string line;
-                        while ((line = sr.ReadLine()) != null)
-                        {
-                            lines.Add(line);
-                        }
-
-                        foreach (var str in lines)
-                        {
-                            Console.WriteLine(str);
-                        }
-                        Console.ReadKey();
-                    }
-                }
-                catch
+                path = documentNameOrPath;
+                if (!File.Exists(path))
                 {
                     Console.WriteLine("Nenhum arquivo com esse nome foi encontrado. Verifique se o nome do arquivo ou caminho dele estão corretos e tente novamente. Pressione ENTER para continuar");
                     Console.ReadKey();
+                    return;
                 }
-
-                Console.WriteLine("Deseja testar com outro arquivo?");
-                Console.WriteLine("1. Sim");
-                Console.WriteLine("2. Não");
-                stillInProgram = Convert.ToInt32(Console.ReadLine());
             }
+            else
+            {
+                path = documentNameOrPath + ".201";
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("Nenhum arquivo com esse nome foi encontrado. Verifique se o nome do arquivo ou caminho dele estão corretos e tente novamente. Pressione ENTER para continuar");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            StreamReader reader;
+
+
+            //Carrega elementos padrões da linguagem.
+            LoadReservedSymbols();
+            LoadReservedTypes();
+            LoadReservedWords();
+
+            reader = new StreamReader(@path);
+
+            do
+            {
+
+            } while (!reader.EndOfStream);
+
+
             Console.WriteLine("Programa finalizado. Pressione ENTER para continuar");
             Console.ReadKey();
+        }
+
+        public static void LoadReservedSymbols()
+        {
+            reservedSymbols.Add("!=", "410");
+            reservedSymbols.Add("#", "410");
+            reservedSymbols.Add("&", "411");
+            reservedSymbols.Add("(", "412");
+            reservedSymbols.Add("/", "413");
+            reservedSymbols.Add(";", "414");
+            reservedSymbols.Add("[", "415");
+            reservedSymbols.Add("{", "416");
+            reservedSymbols.Add("+", "417");
+            reservedSymbols.Add("<=", "418");
+            reservedSymbols.Add("=", "419");
+            reservedSymbols.Add(">=", "420");
+            reservedSymbols.Add("!", "421");
+            reservedSymbols.Add("%", "422");
+            reservedSymbols.Add(")", "423");
+            reservedSymbols.Add("*", "424");
+            reservedSymbols.Add(",", "425");
+            reservedSymbols.Add("]", "426");
+            reservedSymbols.Add("|", "427");
+            reservedSymbols.Add("}", "428");
+            reservedSymbols.Add("<", "429");
+            reservedSymbols.Add("==", "430");
+            reservedSymbols.Add("-", "432");
+        }
+
+        public static void LoadReservedTypes()
+        {
+            reservedTypes.Add("CHARACTER", "510");
+            reservedTypes.Add("CONSTANT-STRING", "511");
+            reservedTypes.Add("FLOAT-NUMBER", "512");
+            reservedTypes.Add("FUNCTION", "513");
+            reservedTypes.Add("IDENTIFIER", "514");
+            reservedTypes.Add("INTEGER-NUMBER", "515");
+        }
+
+        public static void LoadReservedWords()
+        {
+            reservedWords.Add("BOOL", "310");
+            reservedWords.Add("WHILE", "311");
+            reservedWords.Add("BREAK", "312");
+            reservedWords.Add("VOID", "313");
+            reservedWords.Add("CHAR", "314");
+            reservedWords.Add("TRUE", "315");
+            reservedWords.Add("ELSE", "316");
+            reservedWords.Add("STRING", "317");
+            reservedWords.Add("END", "318");
+            reservedWords.Add("RETURN", "319");
+            reservedWords.Add("FALSE", "320");
+            reservedWords.Add("PROGRAM", "321");
+            reservedWords.Add("FLOAT", "322");
+            reservedWords.Add("INT", "323");
+            reservedWords.Add("IF", "324");
+            reservedWords.Add("BEGIN", "325");
         }
     }
 }
